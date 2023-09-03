60da1691f47b10eb40b152ffdcdfddd7988744b4
Fuchsiano <Fuchsiano04@gmail.com>
2023-09-03 17:28:01 +0200

    Fuck neo

--- MiniJamUnity/Assets/scripts/Throwable.cs
+++ MiniJamUnity/Assets/scripts/Throwable.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Throwable : MonoBehaviour
 {
@@ -13,7 +14,8 @@ public class Throwable : MonoBehaviour
 
     [SerializeField]
     public PotionTypes potionType;
-    
+
+
     [SerializeField]
     [Range(1f,4f)]
     private float potionSplashRange;
@@ -26,12 +28,27 @@ public class Throwable : MonoBehaviour
     float curveTime;
     float unitsToTravel;
 
+    [Header("Particles")]
+
+    [SerializeField]
+    public Transform notResetingBackground;
+
+    [SerializeField]
+    private Color particleColor;
+
+    [SerializeField]
+    private ParticleSystem particles;
+
+    [SerializeField]
+    private GameObject particlesPrefab;
+
+    private bool runsCoroutine = false;
     // Start is called before the first frame update
     void Awake()
     {
+
         origin = transform.position;
         curveTime = 0;
-
         unitsToTravel = Vector3.Distance(origin,target) * 2f;
     }
 
@@ -53,16 +70,40 @@ public class Throwable : MonoBehaviour
         // amount to sway to the right
        transform.localPosition = transform.localPosition +  Vector3.left * (throwingCurve.Evaluate(curveTime) * curveAmount * 2 );
 
-        if (curveTime > 1)
+        if (curveTime > 1 && !runsCoroutine)
         {
-            GameObject.Destroy(gameObject);
+            runsCoroutine = true;
+
+            StartCoroutine(PotionParticles());
             Explode();
         }
 
     }
 
+    private IEnumerator PotionParticles()
+    {
+
+        GetComponent<SpriteRenderer>().enabled = false;
+
+
+        particles = GameObject.Instantiate(particlesPrefab,transform.position,transform.rotation,notResetingBackground).GetComponent<ParticleSystem>();
+        var main = particles.main;
+        main.startColor = new MinMaxGradient(particleColor);
+        particles.Play();
+
+        yield return new WaitForSeconds(particles.main.duration);
+        //yield return new WaitForSeconds(0.2f);
+
+        particles.Stop();
+        GameObject.Destroy(particles.gameObject);
+
+        GameObject.Destroy(gameObject);
+
+    }
+
     private void Explode()
     {
+
         Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, potionSplashRange);
 
         for (int i = 0; i < hits.Length; i++)
